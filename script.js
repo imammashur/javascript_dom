6de2503009a998022737a8c123a6e6606fb52ad4
Imam Mashur <58927519+imammashur@users.noreply.github.com>
2020-08-01 10:13:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(document);
+// DOM Selection
+
+// getElementById -> mengembalikan dalam bentuk = Element
+const judul = document.getElementById('judul');
+judul.style.color = 'red';
+judul.style.backgroundColor = '#ccc';
+judul.innerHTML = 'Imam Mashur';
+
+// getElementsByTagName -> mengembalikan dalam bentuk = HTMLCollection
+const p = document.getElementsByTagName('p');
+p[1].style.backgroundColor = 'lightblue';
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = "65px";
+
+// getElementsByClassName -> mengembalikan dalam bentuk = HTMLCollection
+const p2 = document.getElementsByClassName('p2');
+p2[0].innerHTML = "Ini dari JS";
